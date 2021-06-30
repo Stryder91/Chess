988525508e015c0d89c265f1c9396080ac91e4dc
lionel <lioneldg92@gmail.com>
2021-06-30 19:35:55 +0200

    Draw board OK

--- ChessMain.py
+++ ChessMain.py
@@ -4,6 +4,64 @@ Handling user input
 """
 
 import pygame as p
-from Chess import GameState
+from ChessEngine import *
+import ChessEngine
 
-print("hey")
+WIDTH = HEIGHT = 512 
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15 # for animations
+IMAGES = {}
+
+
+def loadImages():
+    pieces = ["wp", "wR", "wN", "wB", "wK", "wQ", "bp", "bR", "bN", "bB", "bK", "bQ"]
+
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+    
+def main():
+    p.init()
+
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+
+    gs = ChessEngine.GameState()
+
+    loadImages() #Only once, before while loop
+
+    running = True
+
+    while running:
+
+        # Pour chaque évènement pygame, si le type de l'évènement vaut p.QUIT
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+def drawGameState(screen, gs):
+    drawBoard(screen) # draw squares on board
+    drawPieces(screen, gs.board) # draw pieces on top of squares
+
+def drawBoard(screen):
+    colors = [p.Color("dark green"), p.Color("dark red")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+if __name__ == '__main__':
+    main()
